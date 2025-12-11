9e1a82764d4db5aaf4747829782411117d7211d8
Manthan Mule <mulemanthan525@gmail.com>
2025-12-11 10:01:08 +0530

    Square of number

---
+++ arithmatic.py
@@ -0,0 +1,7 @@
+def calculate_square():
+    num = input("Enter a number: ")
+    squared = int(num) ** 2
+    print("The square of the number is:", squared)
+
+if __name__ == "__main__":
+    calculate_square()
