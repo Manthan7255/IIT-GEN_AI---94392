72f7ddd9d1262afa2620ba7189459ab9827ca5e1
Manthan Mule <mulemanthan525@gmail.com>
2025-12-11 12:03:07 +0530

    main file

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-import arithmatic
-arithmatic.calculate_square()
+from AssignmentQ1 import sentence
+if __name__ == "__main__":
+    user_sentence = input("Enter a sentence: ")
+    sentence(user_sentence)
+    
