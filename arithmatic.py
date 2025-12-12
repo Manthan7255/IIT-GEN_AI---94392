d95c2d5095ed64f0603a1ecef7d77a44ea0b0138
Manthan7255 <mulemanthan525@gmail.com>
2025-12-12 22:16:49 +0530

    Delete arithmatic.py

--- arithmatic.py
+++
@@ -1,7 +0,0 @@
-def calculate_square():
-    num = input("Enter a number: ")
-    squared = int(num) ** 2
-    print("The square of the number is:", squared)
-
-if __name__ == "__main__":
-    calculate_square()
